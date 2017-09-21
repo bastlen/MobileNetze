402883ec03941f09fa5b97b6bb17b4e8b0a77392
Basti <bast.eln@web.de>
2017-09-21 11:56:38 +0200

    DatabaseAccess **added Methods

--- DatabaseAccess.py
+++ DatabaseAccess.py
@@ -1,17 +1,24 @@
 import sqlite3
 
-
 # database Path
 path = '/home/mone2/openbsc-0.14.0/openbsc/src/osmo-nitb/hlr.sqlite3'
-# connect to DB
-conn = sqlite3.connect(path)
-#conn = sqlite3.connect(db_path)
-cursor = conn.cursor()
 
-cursor.execute("SELECT * FROM Subscriber")
+# establish connection and return cursor obj
+def connect():
+    # connect to DB
+    conn = sqlite3.connect(path)
+    cursor = conn.cursor()
+    return cursor
 
-out = cursor.fetchall()
-strOut = ''
-for x in out:
+cursor = connect()
+
+# get all Subscribers, return list
+def getSubscribers():
+    cursor.execute("SELECT * FROM Subscriber")
+    outList = cursor.fetchall()
+    return outList
+
+
+subscriberList = getSubscribers()
+for x in subscriberList:
     print(x)
-
